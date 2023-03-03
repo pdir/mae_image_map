d508f639e229099f22cd7e6f96f2f4f11e0dfff4
Mathias Arzberger <arzberger@pdir.de>
2023-03-03 08:44:00 +0100

    Fix tl_content.tl_mae_img_map_id SQL definition https://github.com/marebe1602/mae_image_map/pull/1 by https://github.com/fritzmg

--- dca/tl_content.php
+++ dca/tl_content.php
@@ -6,6 +6,6 @@ $GLOBALS['TL_DCA']['tl_content']['fields']['tl_mae_img_map_id'] = array(
     'inputType'               => 'select',
     'foreignKey'              => 'tl_mae_img_map.title',
     'eval'       => array('mandatory'=>true, 'tl_class'=>'w50','chosen'=>true),
-    'sql'        => "int(10) unsigned NOT NULL"
+    'sql'        => "int(10) unsigned NOT NULL default 0"
 );
 $GLOBALS['TL_DCA']['tl_content']['palettes']['mae_img_map'] = '{type_legend},type,headline;{source_legend},singleSRC,tl_mae_img_map_id;{image_legend},alt,title,size,imagemargin;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID,space;{invisible_legend:hide},invisible,start,stop';
